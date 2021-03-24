50d403a60b2d5778d36fad190dc9208a2ead3c24
heatclift77 <adityapratamaputra74@gmail.com>
2021-03-24 17:12:05 +0700

    week 6

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-// const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-// my module
-// const signUp = require('./routers/signUp')
-// const signIn = require('./routers/signIn')
-// const movie = require('./routers/movie')
-// const ticket = require('./routers/tickets')
-// const transactions = require('./routers/transaction')
-// const user = require('./routers/user')
-// const cinema = require('./routers/cinema')
-// const jadwal = require('./routers/jadwal_tayang')
-const router = require('./src/routers')
+// my router
+const router = require("./src/routers");
 // --------
+// module
+// const movie = require("./src/routers/movie");
+// const cinema = require("./src/routers/cinema");
+// const jadwal_tayang = require("./src/routers/jadwal_tayang");
+// 
 
 // midleware
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-// app.use(bodyParser.urlencoded())
-// app.use('/SignUp', signUp)
-// app.use('/SignIn', signIn)
-// app.use('/movie', movie)
-// app.use('/ticket', ticket)
-// app.use('/transaction', transactions)
-// app.use('/user', user)
-// app.use('/cinema', cinema)
-// app.use('/jadwal_tayang', jadwal)
-app.use('/v1', router)
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use("/v1/cinemas", cinema);
+// app.use("/v1/jadwal_tayang", jadwal_tayang);
+app.use("/v1", router);
+app.use('/img', express.static(__dirname + '/src/cover-movie'));
 app.listen(process.env.PORT, ()=>{
     console.log(`running on port ${process.env.PORT}`);
-})
+});
