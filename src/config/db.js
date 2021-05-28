0efdfd2f22c5933671794450a1ac3673687293a1
heatclift77 <adityapratamaputra74@gmail.com>
2021-05-28 17:55:29 +0700

    add password db

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
-    database: process.env.DBS
+    database: process.env.DBS,
+    password:process.env.PASS
 });
 
 module.exports = connection;
